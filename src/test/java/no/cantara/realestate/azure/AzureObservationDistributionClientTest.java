e34ef483fcba16dc7b439f1bc97b6d4dea15abd7
baardl <bard.lind@gmail.com>
2023-09-13 10:57:02 +0200

    #20 Verify ObservedMessage

---
+++ src/test/java/no/cantara/realestate/azure/AzureObservationDistributionClientTest.java
@@ -0,0 +1,19 @@
+package no.cantara.realestate.azure;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AzureObservationDistributionClientTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void publish() {
+    }
+
+    @Test
+    void messageSent() {
+    }
+}
