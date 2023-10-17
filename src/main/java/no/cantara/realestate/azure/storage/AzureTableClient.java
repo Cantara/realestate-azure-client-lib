cb5723ae1213875f166482a536d67f2a96616603
baardl <bard.lind@gmail.com>
2023-10-17 17:49:28 +0200

    #27 Make a good attempt to cast from Object to String

--- src/main/java/no/cantara/realestate/azure/storage/AzureTableClient.java
+++ src/main/java/no/cantara/realestate/azure/storage/AzureTableClient.java
@@ -5,6 +5,8 @@ import com.azure.data.tables.TableClientBuilder;
 import com.azure.data.tables.models.ListEntitiesOptions;
 import org.slf4j.Logger;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -34,8 +36,31 @@ public class AzureTableClient {
     public List<Map<String,Object>> findRows(String partitionKey) {
         ListEntitiesOptions options = new ListEntitiesOptions()
                 .setFilter(String.format("PartitionKey eq '%s'", partitionKey));
+        //Cast from Map<String, Object>     to Map<String, String>
         return tableClient.listEntities(options, null, null).stream()
                 .map(tableEntity -> tableEntity.getProperties())
                 .toList();
+    }
+
+    public List<Map<String,String>> listRows(String partitionKey) {
+        List<Map<String,String>> stringRows = new ArrayList<>();
+        List<Map<String, Object>> rows = findRows(partitionKey);
+        for (Map<String, Object> row : rows) {
+            Map<String,String> stringRow = new HashMap<>();
+            for (String key : row.keySet()) {
+                Object value = row.get(key);
+                if (value instanceof String) {
+                    stringRow.put(key, (String) value);
+                } else {
+                    try {
+                        stringRow.put(key, value.toString());
+                    } catch (Exception e) {
+                        log.error("Could not convert value {} to String", value);
+                        stringRow.put(key, "--FailedInConversionFromAzureTable--");
+                    }
+                }
+            }
+        }
+        return stringRows;
     }
 }
