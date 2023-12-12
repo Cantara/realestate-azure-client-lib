06ece95fa1bac968263b79d56301ce13a1e93c3f
baardl <bard.lind@gmail.com>
2023-12-12 12:20:10 +0100

    Try to send distributed tracing regarding IoT hub

--- src/main/java/no/cantara/realestate/azure/iot/AzureDeviceClient.java
+++ src/main/java/no/cantara/realestate/azure/iot/AzureDeviceClient.java
@@ -2,6 +2,12 @@ package no.cantara.realestate.azure.iot;
 
 import com.microsoft.azure.sdk.iot.device.*;
 import com.microsoft.azure.sdk.iot.device.exceptions.IotHubClientException;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 import org.slf4j.Logger;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -10,13 +16,19 @@ public class AzureDeviceClient {
     private static final Logger log = getLogger(AzureDeviceClient.class);
 
     private final DeviceClient deviceClient;
+    private final Tracer tracer;
 
     private boolean connectionEstablished = false;
     private boolean retryConnection;
+    private String iotHubHostname = "";
 
     public AzureDeviceClient(String connectionString) {
         ClientOptions clientOptions = ClientOptions.builder().keepAliveInterval(30).build();
         deviceClient = new DeviceClient(connectionString, IotHubClientProtocol.MQTT_WS, clientOptions);
+        if (deviceClient != null && deviceClient.getConfig() != null) {
+            iotHubHostname = deviceClient.getConfig().getIotHubHostname();
+        }
+        tracer = GlobalOpenTelemetry.getTracer("no.cantara.realestate");
     }
 
     /*
@@ -24,6 +36,10 @@ public class AzureDeviceClient {
          */
     protected AzureDeviceClient(DeviceClient deviceClient) {
         this.deviceClient = deviceClient;
+        if (deviceClient != null && deviceClient.getConfig() != null) {
+            iotHubHostname = deviceClient.getConfig().getIotHubHostname();
+        }
+        tracer = GlobalOpenTelemetry.getTracer("no.cantara.realestate");
     }
 
     public void openConnection() {
@@ -48,7 +64,39 @@ public class AzureDeviceClient {
 
 
     public void sendEventAsync(Message message, MessageSentCallback messageSentCallback) {
-        deviceClient.sendEventAsync(message, messageSentCallback, message);
+        // Extract the SpanContext and other elements from the request.
+//        Context extractedContext = GlobalOpenTelemetry.getPropagators().getTextMapPropagator()
+//                .extract(Context.current(), httpExchange, getter);
+//        try (Scope scope = extractedContext.makeCurrent()) {
+            // Automatically use the extracted SpanContext as parent.
+            Span span = tracer.spanBuilder("sendEventAsync")
+                    .setSpanKind(SpanKind.CLIENT)
+                    .startSpan();
+            try {
+                // Add the attributes defined in the Semantic Conventions
+                span.setAttribute("destination.address", iotHubHostname); //serverSpan.setAttribute(SemanticAttributes.DESTINATION_ADDRESS, iotHubHostname);
+                span.setAttribute(SemanticAttributes.MESSAGE_TYPE, "SENT");
+
+//                span.setAttribute(SemanticAttributes.MESSAGING_DESTINATION_NAME, "messages/events"); <-- QueueName
+//                serverSpan.setAttribute(SemanticAttributes.HTTP_SCHEME, "http");
+//                serverSpan.setAttribute(SemanticAttributes.HTTP_HOST, "localhost:8080");
+//                serverSpan.setAttribute(SemanticAttributes.HTTP_TARGET, "/resource");
+                // Serve the request
+
+                deviceClient.sendEventAsync(message, messageSentCallback, message);
+            } catch (Exception e) {
+                    span.setStatus(StatusCode.ERROR, "Something bad happened!");
+                    span.recordException(e);
+                    log.error("Failed to send message to Azure IoT Hub", e);
+                    throw e;
+
+            } finally {
+                span.end();
+            }
+//        } catch (Exception e) {
+//            span.setStatus(StatusCode.ERROR, "Something bad happened!");
+//            span.recordException(e);
+//        }
     }
 
     public boolean isConnectionEstablished() {
